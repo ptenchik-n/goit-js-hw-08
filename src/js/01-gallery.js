9fdd384ab1a0201da2b15b81a04c621a01f91315
Anastasiia <nastia.ptenchik11@gmail.com>
2023-11-27 23:28:07 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,55 +1,29 @@
-// Add imports above this line
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
-
-// Change code below this line
+// Add imports above this line
 import { galleryItems } from './gallery-items';
 
-    
-const galleryWharehouse = document.querySelector(".gallery")
+// Change code below this line
 
-galleryWharehouse.insertAdjacentHTML("beforeend", createMarkup(galleryItems))
-galleryWharehouse.addEventListener("click", handleClick)
+const galleryWharehouse = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryWharehouse.insertAdjacentHTML('beforeend', itemsMarkup);
 
-function createMarkup(arr) {
-    return arr.map(({ preview, original, description }) => `
-    <li class="gallery__item">
-  <a class="gallery__link" href="${original}">
+function createGalleryItemsMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__item" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="${original}"
       alt="${description}"
     />
   </a>
-</li>
-    `).join("")
+`
+  }).join('');
 }
 
 
-function handleClick(event) {
-    event.preventDefault();
-
-    if (!event.target.classList.contains("gallery__image")) {
-        return;
-    };
-
-    const originalImageURL = event.target.dataset.source;
-    // console.log(originalImageURL);
-    
-    const lightBox = basicLightbox.create(
-        `<img src="${originalImageURL}" width="800" height="600"/>`
-    );
-    lightBox.show();
-
-    const closeLightBox = (event) => {
-            if (event.key === 'Escape') {
-                lightBox.close();
-                document.removeEventListener('keydown', closeLightBox);
-            }
-        };
-    document.addEventListener('keydown', closeLightBox);
-
-}
-
-console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
